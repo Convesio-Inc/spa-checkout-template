c8cd88ade152b34e7dac68e5741fd8bccb2b87ce
Albert <albert@sirvelia.com>
2026-04-17 18:11:19 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite"
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), tailwindcss(), cloudflare()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
