4bb7685cadaf149123c44acd39dedf84a4d4fc39
Amadeu Arderiu <amadeu@sirvelia.com>
2026-04-21 18:28:19 +0200

    feat: App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { CheckoutPage } from "@/pages/CheckoutPage";
+import { ProductPage } from "@/pages/ProductPage";
+import { ThankYouPage } from "@/pages/ThankYouPage";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
-  return <CheckoutPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CheckoutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
